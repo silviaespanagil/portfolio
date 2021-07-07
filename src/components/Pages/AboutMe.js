50d7c70640ea1a6264c5c26e5270b21a2168e128
silviaespanagil <silviaespanagil@gmail.com>
2021-07-07 20:43:20 +0200

    Change about me text

--- src/components/Pages/AboutMe.js
+++ src/components/Pages/AboutMe.js
@@ -8,17 +8,25 @@ const AboutMe = () => {
             Soy Silvia España , Front End Developer
           </h3>
           <p className="AboutMe__content-paragraph">
-            Tras desempeñarme durante años como especialista en Marketing
-            Digital pasé de diseñar e implementar estrategias de comunicación
-            digital para desarrollar código que te permita comunicar todo el
-            tiempo.
+            Apasionada y curiosa por naturaleza tras varios años desarrollándome
+            en Marketing y formando parte de equipos multidisciplinares, decidí
+            hacer caso a un llamado de curiosidad y pasión y me apunté a un
+            Bootcamp intensivo.
           </p>
-          <p>
-            El mundo laboral me dio la oportunidad de formar parte de varios
-            equipos interdisciplares conseguí mi vocación y pasión en la
-            programación. Ahora programadora Front End estoy aprendiendo a
-            desarrollar código optimizado que permita potenciar la presencia de
-            tu marca a la vez que mejora la experiencia del usuario.
+          <p className="AboutMe__content-paragraph">
+            Pasé de diseñar e implementar estrategias de comunicación digital
+            para desarrollar código que te permita comunicar todo el tiempo.
+          </p>
+          <p className="AboutMe__content-paragraph">
+            En la programación encontré una conjunción de cosas que me gustan:
+            resolver problemas, mejorar experiencias y crear. Ahora estoy en la
+            búsqueda de primera oportunidad laboral como Front End Developer.
+            ¿Creamos algo?
+          </p>
+          <p className="AboutMe__content-paragraph">
+            Cuando no estoy entre líneas de código me puedes conseguir detrás de
+            un libro, jugando videojuegos o haciendo rutas con mi familia y
+            perro.
           </p>
         </div>
       </article>
