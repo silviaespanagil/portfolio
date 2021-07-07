cdde571167e6dcd78efd5d24d80ebddd18854dbf
silviaespanagil <silviaespanagil@gmail.com>
2021-07-07 21:00:29 +0200

    Change palette

--- src/components/Pages/Presentation.js
+++ src/components/Pages/Presentation.js
@@ -1,21 +1,26 @@
 import portrait from "../../images/silvia.png";
+import portrait2 from "../../images/silvia2.png";
 
 const Presentation = () => {
   return (
     <>
       <section className="presentation">
+        <img
+          className="presentation__portrait--img"
+          src={portrait2}
+          alt="Silvia"
+        />
         <div className="text-animation">
           <h2 className="text">Hola mundo, soy Silvia...</h2>
         </div>
-        <div className="NeonSquare" />
-        <div className="NeonBigSquare" />
+        {/*<div className="NeonBigSquare" />
         <div className="presentation__portrait">
-          <img
-            className="presentation__portrait--img"
-            src={portrait}
-            alt="Silvia"
-          />
-        </div>
+        <img
+          className="presentation__portrait--img"
+          src={portrait}
+          alt="Silvia"
+        />
+        </div>*/}
       </section>
     </>
   );
