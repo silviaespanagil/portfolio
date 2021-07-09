acf8d52dba7066860d4b0b3ac02caddd45645614
silviaespanagil <silviaespanagil@gmail.com>
2021-07-09 11:42:56 +0200

    Change presentation to grid and fix for smaller devices

--- src/components/Pages/Presentation.js
+++ src/components/Pages/Presentation.js
@@ -4,22 +4,10 @@ const Presentation = () => {
   return (
     <>
       <section className="presentation">
-        <img
-          className="presentation__portrait--img"
-          src={portrait}
-          alt="Silvia"
-        />
+        <img className="presentation__portrait" src={portrait} alt="Silvia" />
         <div className="text-animation">
           <h2 className="text">Hola mundo, soy Silvia...</h2>
         </div>
-        {/*<div className="NeonBigSquare" />
-        <div className="presentation__portrait">
-        <img
-          className="presentation__portrait--img"
-          src={portrait}
-          alt="Silvia"
-        />
-        </div>*/}
       </section>
     </>
   );
