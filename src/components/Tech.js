12938889eb60cff72251d3faf12eac9e75570650
silviaespanagil <silviaespanagil@gmail.com>
2021-07-10 12:29:10 +0200

    Add titles mixin

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -1,23 +1,24 @@
 const Tech = () => {
   const techClass = [
-    "fab fa-html5",
-    "fab fa-css3-alt",
-    "fab fa-js-square",
-    "fab fa-react",
-    "fab fa-sass",
-    "fab fa-github-alt",
-    "fab fa-node-js",
-    "fab fa-npm",
-    "fab fa-slack",
+    "fab fa-html5 fa-3x",
+    "fab fa-css3-alt fa-3x",
+    "fab fa-js-square fa-3x",
+    "fab fa-react fa-3x",
+    "fab fa-sass fa-3x",
+    "fab fa-github-alt fa-3x",
+    "fab fa-node-js fa-3x",
+    "fab fa-npm fa-3x",
+    "fab fa-slack fa-3x",
   ];
 
   const techs = techClass.map((tech) => {
-    return <i className={tech} />;
+    return <i className={`${tech} tech__items`} />;
   });
+
   return (
     <>
       <section className="tech">
-        <h2>Tecnologías y competencias</h2>
+        <h2 className="tech__title">Tecnologías y competencias</h2>
         <div>{techs}</div>
         <div></div>
       </section>
