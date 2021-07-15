b23b96554685e084a52ccef2d72c098a831411a7
silviaespanagil <silviaespanagil@gmail.com>
2021-07-15 09:10:08 +0200

    Add data at facts.json

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,13 +2,24 @@ import factData from "../data/facts.json";
 
 const Skills = () => {
   const facts = factData.map((fact, index) => {
-    return <li key={index}>{fact.fact}</li>;
+    return (
+      <>
+        <p className="skills__facts--text">{fact.fact}</p>
+        <img
+          src={fact.factImg}
+          className="skills__facts--img"
+          alt="Mis skills"
+        />
+      </>
+    );
   });
   const randomFact = Math.floor(Math.random() * facts.length);
   return (
     <>
-      <h2 className="skills__title">Otras curiosidades...</h2>
-      <p>{facts[randomFact]}</p>
+      <section className="skills">
+        <h2 className="skills__title">Otras curiosidades...</h2>
+        <p className="skills__facts">{facts[randomFact]}</p>
+      </section>
     </>
   );
 };
