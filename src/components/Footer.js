d0578b79aab8836c7aaa861d5c9c3e5d6c46b554
silviaespanagil <silviaespanagil@gmail.com>
2021-07-17 13:13:52 +0200

    Add background to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,21 +4,23 @@ const Footer = () => {
   return (
     <>
       <footer className="footer">
-        <ul className="footer__wrapper">
-          <li className="footer__items">
-            <a
-              href="https://www.linkedin.com/in/silviaespanagil/"
-              alt="Mi LinkedIn"
-              className="footer__items--links"
-            >
-              ¡Hablemos!
-            </a>
-          </li>
-          <li className="footer__items footer__copy">
-            <p className="footer__copy--text">© Silvia España Gil</p>
-            <img className="footer__copy--img" src={silvia} alt="Silvia" />
-          </li>
-        </ul>
+        <div className="footer__background">
+          <ul className="footer__wrapper">
+            <li className="footer__items">
+              <a
+                href="https://www.linkedin.com/in/silviaespanagil/"
+                alt="Mi LinkedIn"
+                className="footer__items--links"
+              >
+                ¡Hablemos!
+              </a>
+            </li>
+            <li className="footer__items footer__copy">
+              <p className="footer__copy--text">© Silvia España Gil</p>
+              <img className="footer__copy--img" src={silvia} alt="Silvia" />
+            </li>
+          </ul>
+        </div>
       </footer>
     </>
   );
