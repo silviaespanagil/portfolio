d6e34d7b2463e944c0c310b44596c9f4e8ca103b
silviaespanagil <silviaespanagil@gmail.com>
2021-07-21 12:32:37 +0200

    Add ScrollPosition and visible button function to go up

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { useState, useEffect } from "react";
 //COMPONENTS
 import Presentation from "./Presentation";
+import GoTop from "./GoTop";
 import AboutMe from "./AboutMe";
 import ProjectList from "./ProjectList";
 import Tech from "./Tech";
@@ -10,9 +12,30 @@ import Footer from "./Footer";
 import "../stylesheets/App.scss";
 
 function App() {
+  //Scroll position that shows/hide "top" button
+
+  let [showGoTop, setshowGoTop] = useState("goTopHidden");
+  const [scrollPosition, setSrollPosition] = useState(0);
+
+  const handleVisibleButton = () => {
+    const position = window.pageYOffset;
+    setSrollPosition(position);
+
+    if (scrollPosition > 50) {
+      return setshowGoTop("goTop");
+    } else if (scrollPosition < 50) {
+      return setshowGoTop("goTopHidden");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleVisibleButton);
+  });
+
   return (
     <>
       <Presentation />
+      <GoTop showGoTop={showGoTop} />
       <AboutMe />
       <ProjectList />
       <Tech />
---
+++ src/components/GoTop.js
@@ -0,0 +1,12 @@
+const GoTop = (props) => {
+  return (
+    <>
+      <div className={props.showGoTop}>
+        <section className="goTop">
+          <p className="goTop__text">Top</p>
+        </section>
+      </div>
+    </>
+  );
+};
+export default GoTop;
