11ffce5817a7bb9dd991d4c3e4b5af4a313eed9a
silviaespanagil <silviaespanagil@gmail.com>
2021-07-24 16:20:21 +0200

    Addshuffle option for random facts

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import { useState } from "react";
 //SERVICES
 import factData from "../data/facts.json";
 
 const Facts = () => {
+  const [factNumber, setFactNumber] = useState("1");
+
   //Mapping of facts JSON
   const facts = factData.map((fact, index) => {
     return <>{fact.fact}</>;
@@ -11,15 +13,17 @@ const Facts = () => {
 
   //Random number function to take an index number to render
 
-  const randomFact = () => {
-    return Math.floor(Math.random() * facts.length);
-  };
-
+  function randomFact() {
+    return setFactNumber(Math.floor(Math.random() * facts.length));
+  }
   return (
     <div className="presentation__facts">
       <p className="presentation__facts--fact">
-        Una curiosidad sobre mi: {facts[randomFact()]}
+        Una curiosidad sobre mi: {facts[factNumber]}
       </p>
+      <div>
+        <i class="fas fa-redo" onClick={randomFact}></i>
+      </div>
     </div>
   );
 };
