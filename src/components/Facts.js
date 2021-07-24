32ef7bdb4a1ed498d75ad577a322eb6d01b1e4c7
silviaespanagil <silviaespanagil@gmail.com>
2021-07-24 17:15:56 +0200

    Add styles to shuffle button

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 import factData from "../data/facts.json";
 
 const Facts = () => {
-  const [factNumber, setFactNumber] = useState("1");
+  const [factNumber, setFactNumber] = useState("0");
 
   //Mapping of facts JSON
   const facts = factData.map((fact, index) => {
@@ -21,9 +21,11 @@ const Facts = () => {
       <p className="presentation__facts--fact">
         Una curiosidad sobre mi: {facts[factNumber]}
       </p>
-      <div>
-        <i class="fas fa-redo" onClick={randomFact}></i>
-      </div>
+
+      <i
+        className="fas fa-redo presentation__facts--shuffle"
+        onClick={randomFact}
+      ></i>
     </div>
   );
 };
